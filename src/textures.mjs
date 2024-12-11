1819046241b86836948a75428c9da79c66c5a54d
camsik <cameron_sikich@browm.edu>
2024-12-11 18:01:42 -0500

    cleaning up textures

--- src/textures.mjs
+++ src/textures.mjs
@@ -2,21 +2,21 @@ import * as THREE from 'three';
 
 //TEXTURE 1
 const textureLoader = new THREE.TextureLoader();
-const texture1 = textureLoader.load('src/textures/caveTexture.jpg');
+const texture1 = textureLoader.load('src/textures/stoneCave.jpg');
 texture1.wrapS = THREE.RepeatWrapping;
 texture1.wrapT = THREE.RepeatWrapping;
 texture1.repeat.set(1, 1);
 
 //TEXTURE 2
 const textureLoader2 = new THREE.TextureLoader();
-const texture2 = textureLoader2.load('src/textures/iceCave3.jpeg');
+const texture2 = textureLoader2.load('src/textures/iceCave.jpeg');
 texture2.wrapS = THREE.RepeatWrapping;
 texture2.wrapT = THREE.RepeatWrapping;
 texture2.repeat.set(1, 1);
 
 //TEXTURE 3
 const textureLoader3 = new THREE.TextureLoader();
-const texture3 = textureLoader3.load('src/textures/caveTexture3.jpeg');
+const texture3 = textureLoader3.load('src/textures/crystalCave.jpeg');
 texture3.wrapS = THREE.RepeatWrapping;
 texture3.wrapT = THREE.RepeatWrapping;
 texture3.repeat.set(1, 1);

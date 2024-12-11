8cd362b110b0e16bc9acb5655635483a55196129
jzhang0503 <90428413+jzhang0503@users.noreply.github.com>
2024-12-11 03:54:17 -0500

    L-systems

--- readme.md
+++ readme.md
@@ -1,1 +1,13 @@
 Run with 'npx vite'
+
+# L- Systems
+
+A Lindenmayer system (L System for short) is a rewriting system that allows us to model the "growth" of systems starting from a seed axiom.
+
+L systems are usually represented in 2D such as the Koch snowflake or the fractal tree, but to make this more interesting visually, we expand this into 3D by adding more rules to describe rotations about different axes.
+
+Reference: https://algorithmicbotany.org/papers/abop/abop-ch1.pdf
+
+We used the above paper to inspire some of our L systems by taking already established L systems in 2d then modifying them to create a different one in 3D.
+
+To use the L-system, we generate an L-system string with `generateLSystem`, then pass that into `drawLSystem` along with other parameters like the geometry, positions, rotation angle, etc.
---
+++ src/lsystems/lsystem.js
@@ -0,0 +1,113 @@
+import * as THREE from 'three';
+
+
+/**
+* Generates an L-System string given an axiom, rules, and number of iterations
+* @param {string} axiom - The starting string
+* @param {Object} rules - The rules to apply to the axiom
+* @param {number} iterations - The number of iterations to apply the rules
+*/
+export function generateLSystem(axiom, rules, iterations) {
+    let result = axiom;
+    for (let i = 0; i < iterations; i++) {
+        let nextResult = '';
+        for (let char of result) {
+            nextResult += rules[char] || char;
+        }
+        result = nextResult;
+    }
+    return result;
+}
+
+/**
+ * Draws an L-System in your scene
+ * 
+ * @param {THREE.Scene} scene - The scene to add the L-System to
+ * @param {string} lSystem - The L-System string
+ * @param {number} length - The length of each segment
+ * @param {number} angle - The angle to rotate in radians
+ * @param {THREE.Vector3} position - The starting position
+ * @param {THREE.Geometry} geometry - The geometry to use for each segment
+ * @param {THREE.Material} gradient - The material to use for each segment
+ */
+export function drawLSystem(scene, lSystem, length, angle, position = new THREE.Vector3(0, 0, 0), geometry = new THREE.ConeGeometry(.05, .1, 4), gradient = new THREE.MeshPhysicalMaterial({
+    color: 0x0000ff,
+    metalness: 0.5,
+    roughness: 0.5,
+    clearcoat: 1,
+    clearcoatRoughness: 0.1,
+    reflectivity: 1,
+    envMapIntensity: 1,
+    side: THREE.DoubleSide
+})) {
+    const stack = [];
+    let direction = new THREE.Vector3(1, 0, 0);
+    let quaternion = new THREE.Quaternion();
+
+    for (let char of lSystem) {
+        switch (char) {
+            case 'F': // Draw a shape
+                const box = new THREE.Mesh(geometry, gradient);
+                box.position.copy(position);
+                box.quaternion.copy(quaternion);
+                scene.add(box);
+                position.add(direction.clone().multiplyScalar(length));
+                break;
+            case '+': // Rotate clockwise
+                quaternion.multiplyQuaternions(new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 0, 1), angle), quaternion);
+                direction.applyAxisAngle(new THREE.Vector3(0, 0, 1), angle);
+                break;
+            case '-': // Rotate counterclockwise
+                quaternion.multiplyQuaternions(new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 0, 1), -angle), quaternion);
+                direction.applyAxisAngle(new THREE.Vector3(0, 0, 1), -angle);
+                break;
+            case '&': // Rotate up
+                quaternion.multiplyQuaternions(new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), angle), quaternion);
+                direction.applyAxisAngle(new THREE.Vector3(0, 1, 0), angle);
+                break;
+            case '^': // Rotate down
+                quaternion.multiplyQuaternions(new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), -angle), quaternion);
+                direction.applyAxisAngle(new THREE.Vector3(0, 1, 0), -angle);
+                break;
+            case '\\': // Rotate right
+                quaternion.multiplyQuaternions(new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(1, 0, 0), angle), quaternion);
+                direction.applyAxisAngle(new THREE.Vector3(1, 0, 0), angle);
+                break;
+            case '/': // Rotate left
+                quaternion.multiplyQuaternions(new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(1, 0, 0), -angle), quaternion);
+                direction.applyAxisAngle(new THREE.Vector3(1, 0, 0), -angle);
+                break;
+            case '[': // Save state
+                stack.push({ position: position.clone(), direction: direction.clone(), quaternion: quaternion.clone() });
+                break;
+            case ']':// Restore state
+                const state = stack.pop();
+                position.copy(state.position);
+                direction.copy(state.direction);
+                quaternion.copy(state.quaternion);
+                break;
+        }
+    }
+}
+
+/**
+ * 
+ * @param {THREE.Scene} scene - The scene to add the L-System to
+ * @param {THREE.Vector3} pos - position of the object
+ * @param {number} gen - number of generations(int)
+ */
+export function basicCrystal(scene,pos, gen){
+     const boxGeometry = new THREE.BoxGeometry(.3,.3,.3);
+
+
+    const lSystem = generateLSystem('F', { 'F': 'F[+F][-F]^F[F]\\F/F' }, gen);
+    drawLSystem(scene, lSystem, 0.2, Math.PI / 2, pos,boxGeometry);
+}
+
+export function basicTree(scene,pos, gen){
+    const cylinderGeometry = new THREE.CylinderGeometry(.05, .05, .1, 32);
+    cylinderGeometry.rotateZ(Math.PI / 2);
+
+    const lSystem2 = generateLSystem('+++X', { 'X': '&+F[^X][&X]FX', 'F': 'FF' }, gen);
+    drawLSystem(scene, lSystem2, 0.1, .39, pos, cylinderGeometry);
+}
